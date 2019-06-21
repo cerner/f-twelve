4d1424a2ba2f29fbc693f74e08d0e1678b07524c
Patrick Gross <pgross41@gmail.com>
2019-06-21 01:19:01 -0500

    Enforce 100% code coverage (#10)

--- test/setup.js
+++ test/setup.js
@@ -1,13 +1,15 @@
 import FTwelve from '../src/js/views/f-twelve';
 import JsDomGlobal from 'jsdom-global';
+import { Element } from 'jsdom/lib/jsdom/living';
 
 // Setup environment
 before(function() {
   process.env.NODE_DISABLE_COLORS = true;
   this.jsDomGlobalCleanup = new JsDomGlobal('', { url: 'http://localhost/' });
+  Element.prototype.scrollIntoView = () => null;
   this.fTwelve = new FTwelve();
   this.isAttached = (el) => document.body.contains(el);
-  this.setupError = 'Unable to complete test, setup failed';
+  this.setupError = 'Unable to complete test, prerequisite condition failed';
   this.dispatchKeyboardEvent = (type, key, target) => {
     (target || window.document).dispatchEvent(new KeyboardEvent(type, { key: key }));
   };
