9ead5f914d32c596d7cdf5afc74d304637d4daf2
Patrick Gross <Patrick.Gross@Cerner.com>
2020-09-22 01:38:54 -0500

    Make toggle icon

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Console from './tabs/console/Console';
  * Root app view
  */
 export default ({ id }) => {
+  let app;
   let contentWrapper;
   let content;
 
@@ -22,18 +23,23 @@ export default ({ id }) => {
     }
   };
 
+  const open = () => app.classList.add(styles.open);
+  const close = () => app.classList.remove(styles.open);
+
   const console = Console();
 
   return {
     console,
     el: (
-      <div className={styles.fTwelve} id={id}>
-        <div className={styles.tabBar}>
-          <div className={styles.tab} onclick={() => setContent(<div>toggle</div>)}><Icon className={styles.icon}/>
+      <div className={styles.fTwelve} id={id} ref={el => (app = el)}>
+        <Icon className={styles.closedIcon} onclick={open} title="Show F-Twelve"/>
+        <div className={styles.main}>
+          <div className={styles.tabBar}>
+            <Icon className={styles.openIcon} onclick={close} title="Hide F-Twelve"/>
+            <div className={styles.tab} onclick={() => setContent(console.el)}>Console</div>
           </div>
-          <div className={styles.tab} onclick={() => setContent(console.el)}>Console</div>
+          <div ref={el => (contentWrapper = el)}/>
         </div>
-        <div ref={el => (contentWrapper = el)}/>
       </div>
     )
   };
