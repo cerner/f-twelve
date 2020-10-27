8b847ee23da92206d6e5a019dc65b5032c985e37
Patrick Gross <Patrick.Gross@Cerner.com>
2020-10-27 07:17:05 -0500

    Add toggle parsed button

--- src/components/tabs/network/RequestDetails.js
+++ src/components/tabs/network/RequestDetails.js
@@ -2,6 +2,9 @@ import { h } from 'preact';
 import styles from './RequestDetails.module.scss';
 import ResponseStatus from './ResponseStatus';
 import Tree from '../../dataTree/Tree';
+import { useState } from 'preact/hooks';
+
+const noneDiv = <div className={styles.none}>(none)</div>;
 
 /**
  * Request details
@@ -13,13 +16,13 @@ export default ({ request }) => {
       <div className={styles.header}>Request</div>
       <NameValue name="Method" value={request.method}/>
       <NameValue name="URL" value={request.url}/>
-      <Headers headers={request.headers}/>
-      <Data data={request.data}/>
+      <Headers parsed={request.headers} raw={request.headersRaw}/>
+      <Data data={request.data} raw={request.data}/>
       <hr/>
       <div className={styles.header}>Response</div>
       <NameValue name="Status" value={<ResponseStatus code={request.responseStatus}/>}/>
-      <Headers headers={request.responseHeaders}/>
-      <Data data={request.response || request.responseText}/>
+      <Headers parsed={request.responseHeaders} raw={request.responseHeadersRaw}/>
+      <Data data={request.response} raw={request.responseText}/>
     </div>
   );
 };
@@ -38,20 +41,46 @@ const NameValue = ({ name, value, child }) => (
 /**
  * A NameValue with child NameValues for each header
  */
-const Headers = ({ headers }) => {
-  const isEmpty = typeof headers !== 'object' || !Object.keys(headers).length;
-  const value = isEmpty && <div className={styles.none}>(none)</div>;
-  const child = !isEmpty && Object.keys(headers)
-    .map(key => <NameValue name={key} value={headers[key]}/>);
-  return <NameValue child={child} name="Headers" value={value}/>;
+const Headers = ({ parsed, raw }) => {
+  const [isParsed, toggleParsedButton] = useToggleParsedButton();
+  const isEmpty = typeof parsed !== 'object' || !Object.keys(parsed).length;
+  const value = isEmpty ? noneDiv : toggleParsedButton;
+  const child = !isParsed ? <pre>{raw}</pre> : Object.keys(parsed)
+    .map(key => <NameValue name={key} value={parsed[key]}/>);
+  return <NameValue child={!isEmpty && child} name="Headers" value={value}/>;
 };
 
 /**
  * A NameValue that handles dynamic data of various types
  */
-const Data = ({ data }) => {
-  const value = typeof data === 'object'
-    ? <Tree data={data}/>
-    : data;
-  return <NameValue name="Data" value={value}/>;
+const Data = ({ data, raw }) => {
+  const [isParsed, toggleParsedButton] = useToggleParsedButton();
+  const parsed = typeof data === 'object' ? data : (parse(data) || parse(raw));
+  const value = isParsed ? <Tree data={parsed}/> : <pre>{raw}</pre>;
+  const toggleButton = parsed && toggleParsedButton;
+  return <NameValue child={value} name="Data" value={value ? toggleButton : noneDiv}/>;
+};
+
+/**
+ * Provide a toggle button and the toggle state
+ */
+const useToggleParsedButton = () => {
+  const [isParsed, setIsParsed] = useState(false);
+  const toggleParsedButton = (
+    <div className={styles.toggleParsedButton} onClick={() => setIsParsed(!isParsed)}>
+      {isParsed ? 'view raw' : 'view parsed'}
+    </div>
+  );
+  return [isParsed, toggleParsedButton];
+};
+
+/**
+ * Attempt to parse JSON data
+ */
+const parse = (data) => {
+  try {
+    return JSON.parse(data);
+  } catch (error) {
+    return null;
+  }
 };
